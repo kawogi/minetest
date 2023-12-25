393412fc9f34dc12565de6c3e0ce685faf4f4540
kawogi <github@kai.giebeler.de>
2023-12-25 05:36:48 +0100

    Remove OpenGL ES1

--- src/client/renderingengine.cpp
+++ src/client/renderingengine.cpp
@@ -276,7 +276,6 @@ std::vector<video::E_DRIVER_TYPE> RenderingEngine::getSupportedVideoDrivers()
 	static const video::E_DRIVER_TYPE glDrivers[] = {
 		video::EDT_OPENGL,
 		video::EDT_OGLES2,
-		video::EDT_OGLES1,
 		video::EDT_NULL,
 	};
 	std::vector<video::E_DRIVER_TYPE> drivers;
@@ -311,7 +310,6 @@ const VideoDriverInfo &RenderingEngine::getVideoDriverInfo(irr::video::E_DRIVER_
 	static const std::unordered_map<int, VideoDriverInfo> driver_info_map = {
 		{(int)video::EDT_NULL,   {"null",   "NULL Driver"}},
 		{(int)video::EDT_OPENGL, {"opengl", "OpenGL"}},
-		{(int)video::EDT_OGLES1, {"ogles1", "OpenGL ES1"}},
 		{(int)video::EDT_OGLES2, {"ogles2", "OpenGL ES2"}},
 	};
 	return driver_info_map.at((int)type);
