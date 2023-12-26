bec7a3c379aea1ca99376e3405d5b91bbdc90681
kawogi <github@kai.giebeler.de>
2023-12-26 04:23:38 +0100

    remove unneeded irrlicht files

--- lib/irrlichtmt-server/include/irrunpack.h
+++
@@ -1,9 +0,0 @@
-// Copyright (C) 2007-2012 Nikolaus Gebhardt
-// This file is part of the "Irrlicht Engine".
-// For conditions of distribution and use, see copyright notice in irrlicht.h
-
-// include this file to switch back to default alignment
-// file belongs to irrpack.h, see there for more info
-
-#undef PACK_STRUCT
-
